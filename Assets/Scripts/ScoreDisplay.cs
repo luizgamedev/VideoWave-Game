90eb00ccd301e5fee7e20121c391c710691fa978
Luiz Santos <santos.uff@gmail.com>
2017-01-22 09:05:28 +0000

    Fixing camera issues, putting UI, Adding Score

---
+++ Assets/Scripts/ScoreDisplay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDisplay : MonoBehaviour {
+
+	[SerializeField]
+	Camera m_mainCamera;
+	
+	[SerializeField]
+	Text m_displayText;
+
+	// Use this for initialization
+	void Start () 
+	{
+		m_mainCamera = Camera.main;
+	}
+
+	void LateUpdate()
+	{
+		m_displayText.text = "" + (int)m_mainCamera.transform.position.x;
+	}
+	
+}
